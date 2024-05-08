62c587440caf6c303774db9bcbb107e2d7532ce2
Shanmukh459 <saikrrish459@gmail.com>
2024-05-07 20:32:01 -0700

    extracted binnedData from the actual data

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
+import { bin, extent, scaleLinear, scaleTime, sum, timeMonths } from "d3"
 import { useData } from "./useData"
 
+const height = 500
+const width = 960
+
+const margin = {
+  top: 20,
+  right: 20,
+  bottom: 20,
+  left: 20
+}
 
 function App() {
   const data = useData()
@@ -8,9 +18,37 @@ function App() {
     return <h1>Loading...</h1>
   }
 
-  console.log(data)
+  const innerHeight = height - margin.top - margin.bottom
+  const innerWidth = width - margin.left - margin.right
 
-  return <h1>Missing migrants Aggregation!</h1>
+  const xValue = d => d['Reported Date']
+  const yValue = d => d['Total Dead and Missing']
+
+  const xScale = scaleTime()
+    .domain(extent(data, xValue))
+    .range([0, innerWidth])
+    .nice()
+
+  const [start, stop] = xScale.domain()
+  
+  const binnedData = bin()
+    .value(xValue)
+    .domain(xScale.domain())
+    .thresholds(timeMonths(start, stop))(data)
+    .map(array => ({
+      totalDeadAndMissing: sum(array, yValue),
+      x0: array.x0,
+      x1: array.x1
+    }))
+
+  console.log(binnedData)
+
+  // const yScale = scaleLinear()
+  //   .domain([0, sum(binnedData, d => d.)])
+  return (
+    <svg>
+      <circle></circle>
+    </svg>
+  )
 }
-
 export default App
